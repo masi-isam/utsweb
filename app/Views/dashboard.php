b1cccc641b092e7c3c1f60e31fdef8216cfd80cd
masi-isam <mochisam09@gmail.com>
2023-05-14 14:18:14 +0700

    UTS WEB

---
+++ app/Views/dashboard.php
@@ -0,0 +1,224 @@
+<?= $this->extend('layout'); ?>
+<?= $this->section('bodycontent'); ?>
+
+<div class="page-content">
+    <div class="container">
+        <div class="cover shadow-lg bg-white">
+            <div class="cover-bg p-3 p-lg-4 text-white">
+                <div class="row">
+                    <div class="col-lg-4 col-md-5">
+                        <div class="avatar hover-effect bg-white p-1"><img src="assets/images/avatar.jpg" width="200"
+                                height="200" /></div>
+                    </div>
+                    <div class="col-lg-8 col-md-7 text-center text-md-start">
+                        <h2 class="h1 mt-2" data-aos="fade-left" data-aos-delay="0">Moch. Nur Isyam</h2>
+                        <p data-aos="fade-left" data-aos-delay="100">Hamba Allah</p>
+                        <div class="d-print-none" data-aos="fade-left" data-aos-delay="200"><a
+                                class="btn btn-light text-dark shadow-sm mt-1 me-1" href="right-resume.pdf"
+                                target="_blank">Download CV</a><a class="btn btn-success shadow-sm mt-1"
+                                href="#contact">Hire Me</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="about-section pt-4 px-3 px-lg-4 mt-1">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="h3 mb-3">About Me</h2>
+                        <p>Hello! I’m Moch. Nur Isyam. I am a student at the Muhammadiyah University of Sukabumi. I am
+                            interested in UI/UX design and Web Design.
+                            My hobby is playing games, especially mobile and console games, I also like watching anime
+                            and I often do playing futsal.</p>
+                    </div>
+                    <div class="col-md-5 offset-md-1">
+                        <div class="row mt-2">
+                            <div class="col-sm-4">
+                                <div class="pb-1">Age</div>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="pb-1 text-secondary">21</div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="pb-1">Email</div>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="pb-1 text-secondary">mochisam09@gmail.com</div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="pb-1">Phone</div>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="pb-1 text-secondary">+62-8218-111-XXXX</div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="pb-1">Address</div>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="pb-1 text-secondary">Nagrak, Sukabumi, Indonesia</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="d-print-none" />
+            <div class="skills-section px-3 px-lg-4">
+                <h2 class="h3 mb-3">Skills & Hoby</h2>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-2"><span>HTML</span>
+                            <div class="progress my-1">
+                                <div class="progress-bar bg-primary" role="progressbar" data-aos="zoom-in-right"
+                                    data-aos-delay="100" data-aos-anchor=".skills-section" style="width: 90%"
+                                    aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                        <div class="mb-2"><span>CSS</span>
+                            <div class="progress my-1">
+                                <div class="progress-bar bg-primary" role="progressbar" data-aos="zoom-in-right"
+                                    data-aos-delay="200" data-aos-anchor=".skills-section" style="width: 85%"
+                                    aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                        <div class="mb-2"><span>JavaScript</span>
+                            <div class="progress my-1">
+                                <div class="progress-bar bg-primary" role="progressbar" data-aos="zoom-in-right"
+                                    data-aos-delay="300" data-aos-anchor=".skills-section" style="width: 75%"
+                                    aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-2"><span>Adobe Photoshop</span>
+                            <div class="progress my-1">
+                                <div class="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right"
+                                    data-aos-delay="400" data-aos-anchor=".skills-section" style="width: 80%"
+                                    aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                        <div class="mb-2"><span>Sketch</span>
+                            <div class="progress my-1">
+                                <div class="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right"
+                                    data-aos-delay="500" data-aos-anchor=".skills-section" style="width: 85%"
+                                    aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                        <div class="mb-2"><span>Adobe XD</span>
+                            <div class="progress my-1">
+                                <div class="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right"
+                                    data-aos-delay="600" data-aos-anchor=".skills-section" style="width: 75%"
+                                    aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="d-print-none" />
+            <div class="work-experience-section px-3 px-lg-4">
+                <h2 class="h3 mb-4">Work Experience</h2>
+                <div class="timeline">
+                    <div class="timeline-card timeline-card-primary card shadow-sm">
+                        <div class="card-body">
+                            <div class="h5 mb-1">Web Developer <span class="text-muted h6">at Agribusiness University
+                                    Muhammadiyah Sukabumi</span></div>
+                            <div class="text-muted text-small mb-2">May, 2021</div>
+                            <div>creating e-commerce for the agribusiness department of
+                                Muhammadiyah University of Sukabumi to sell their organic vegetable products.</div>
+                        </div>
+                    </div>
+                    <div class="timeline-card timeline-card-primary card shadow-sm">
+                        <div class="card-body">
+                            <div class="h5 mb-1">Microcontroller <span class="text-muted h6">for campus
+                                    assignments</span>
+                            </div>
+                            <div class="text-muted text-small mb-2">November, 2022</div>
+                            <div>Make a microcontroller project for the final project of the microcontroller course.
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="timeline-card timeline-card-primary card shadow-sm">
+                        <div class="card-body">
+                            <div class="h5 mb-1">Junior Web Developer <span class="text-muted h6">at Indie Studio</span>
+                            </div>
+                            <div class="text-muted text-small mb-2">Jan, 2011 - May, 2013</div>
+                            <div>User generated content in real-time will have multiple touchpoints for offshoring.
+                                Organically grow the holistic world view of disruptive innovation via workplace
+                                diversity and empowerment.</div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+            <hr class="d-print-none" />
+            <!-- <div class="page-break"></div>
+            <div class="education-section px-3 px-lg-4 pb-4">
+                <h2 class="h3 mb-4">Education</h2>
+                <div class="timeline">
+                    <div class="timeline-card timeline-card-success card shadow-sm">
+                        <div class="card-body">
+                            <div class="h5 mb-1">Masters in Information Technology <span class="text-muted h6">from
+                                    International University</span></div>
+                            <div class="text-muted text-small mb-2">2011 - 2013</div>
+                            <div>Leverage agile frameworks to provide a robust synopsis for high level overviews.
+                                Iterative approaches to corporate strategy foster collaborative thinking to further the
+                                overall value proposition.</div>
+                        </div>
+                    </div>
+                    <div class="timeline-card timeline-card-success card shadow-sm">
+                        <div class="card-body">
+                            <div class="h5 mb-1">Bachelor of Computer Science <span class="text-muted h6">from Regional
+                                    College</span></div>
+                            <div class="text-muted text-small mb-2">2007 - 2011</div>
+                            <div>Override the digital divide with additional clickthroughs from DevOps. Nanotechnology
+                                immersion along the information highway will close the loop on focusing solely on the
+                                bottom line.</div>
+                        </div>
+                    </div>
+                    <div class="timeline-card timeline-card-success card shadow-sm">
+                        <div class="card-body">
+                            <div class="h5 mb-1">Science and Mathematics <span class="text-muted h6">from Mt. High
+                                    Scool</span></div>
+                            <div class="text-muted text-small mb-2">1995 - 2007</div>
+                            <div>User generated content in real-time will have multiple touchpoints for offshoring.
+                                Organically grow the holistic world view of disruptive innovation via workplace
+                                diversity and empowerment.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="d-print-none" /> -->
+            <div class="contant-section px-3 px-lg-4 pb-4" id="contact">
+                <h2 class="h3 text mb-3">Contact</h2>
+                <div class="row">
+                    <div class="col-md-7 d-print-none">
+                        <div class="my-2">
+                            <form action="https://formspree.io/your@email.com" method="POST">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <input class="form-control" type="text" id="name" name="name"
+                                            placeholder="Your Name" required>
+                                    </div>
+                                    <div class="col-6">
+                                        <input class="form-control" type="email" id="email" name="_replyto"
+                                            placeholder="Your E-mail" required>
+                                    </div>
+                                </div>
+                                <div class="form-group my-2">
+                                    <textarea class="form-control" style="resize: none;" id="message" name="message"
+                                        rows="4" placeholder="Your Message" required></textarea>
+                                </div>
+                                <button class="btn btn-primary mt-2" type="submit">Send</button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="mt-2">
+                            <h3 class="h6">Address</h3>
+                            <div class="pb-2 text-secondary">Nagrak, Sukabumi, Indonesia</div>
+                            <h3 class="h6">Phone</h3>
+                            <div class="pb-2 text-secondary">+0718-111-0011</div>
+                            <h3 class="h6">Email</h3>
+                            <div class="pb-2 text-secondary">mochisam09@gmail.com</div>
+                        </div>
+                    </div>
+
+
+                    <?= $this->endSection(); ?>
---
+++ app/Views/layout.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>UTS WEB</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin" />
+  <link rel="preload" as="style"
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&amp;family=Roboto:wght@300;400;500;700&amp;display=swap" />
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&amp;family=Roboto:wght@300;400;500;700&amp;display=swap"
+    media="print" onload="this.media='all'" />
+  <noscript>
+    <link rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&amp;family=Roboto:wght@300;400;500;700&amp;display=swap" />
+  </noscript>
+  <link href="../assets/css/font-awesome/css/all.min.css?ver=1.2.0" rel="stylesheet">
+  <link href="../assets/css/bootstrap.min.css?ver=1.2.0" rel="stylesheet">
+  <link href="../assets/css/aos.css?ver=1.2.0" rel="stylesheet">
+  <link href="../assets/css/main.css?ver=1.2.0" rel="stylesheet">
+  <noscript>
+    <style type="text/css">
+      [data-aos] {
+        opacity: 1 !important;
+        transform: translate(0) scale(1) !important;
+      }
+    </style>
+  </noscript>
+</head>
+
+<body id="top">
+  <header class="d-print-none">
+    <div class="container text-center text-lg-left">
+      <div class="py-3 clearfix">
+        <h1 class="site-title mb-0">Moch. Nur Isyam</h1>
+        <div class="site-nav">
+          <nav role="navigation">
+            <ul class="nav justify-content-center">
+              <!-- <li class="nav-item"><a class="nav-link" href="https://twitter.com/templateflip" title="Twitter"><i
+                    class="fab fa-twitter"></i><span class="menu-title sr-only">Twitter</span></a>
+              </li> -->
+              <li class="nav-item"><a class="nav-link" target="_blank" href="https://www.facebook.com/mnur.isam"
+                  title="Facebook"><i class="fab fa-facebook"></i><span class="menu-title sr-only">Facebook</span></a>
+              </li>
+              <li class="nav-item"><a class="nav-link" target="_blank" href="https://www.instagram.com/mnurisyam/"
+                  title="Instagram"><i class="fab fa-instagram"></i><span
+                    class="menu-title sr-only">Instagram</span></a>
+              </li>
+              <li class="nav-item"><a class="nav-link" target="_blank" href="https://github.com/masi-isam"
+                  title="Github"><i class="fab fa-github"></i><span class="menu-title sr-only">Github</span></a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Render section -->
+  <?= $this->renderSection('bodycontent'); ?>
+
+
+
+
+
+  <div class="col d-none d-print-block">
+    <div class="mt-2">
+      <div>
+        <div class="mb-2">
+          <div class="text-dark"><i class="fab fa-twitter mr-1"></i><span>https://twitter.com/templateflip</span>
+          </div>
+        </div>
+        <div class="mb-2">
+          <div class="text-dark"><i class="fab fa-facebook mr-1"></i><span>https://www.facebook.com/templateflip</span>
+          </div>
+        </div>
+        <div class="mb-2">
+          <div class="text-dark"><i
+              class="fab fa-instagram mr-1"></i><span>https://www.instagram.com/templateflip</span>
+          </div>
+        </div>
+        <div class="mb-2">
+          <div class="text-dark"><i class="fab fa-github mr-1"></i><span>https://github.com/templateflip</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  <footer class="pt-4 pb-4 text-muted text-center d-print-none">
+    <div class="container">
+      <div class="my-3">
+        <div class="h4">Thank You</div>
+        <div class="footer-nav">
+          <nav role="navigation">
+            <ul class="nav justify-content-center">
+              <li class="nav-item"><a class="nav-link" href="https://www.facebook.com/mnur.isam" target="_blank"
+                  title="Facebook"><i class="fab fa-facebook"></i><span class="menu-title sr-only">Facebook</span></a>
+              </li>
+              <li class="nav-item"><a class="nav-link" href="https://www.instagram.com/mnurisyam/" target="_blank"
+                  title="Instagram"><i class="fab fa-instagram"></i><span
+                    class="menu-title sr-only">Instagram</span></a>
+              </li>
+              <li class="nav-item"><a class="nav-link" href="https://github.com/masi-isam" target="_blank"
+                  title="Github"><i class="fab fa-github"></i><span class="menu-title sr-only">Github</span></a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+  <script src="../assets/scripts/bootstrap.bundle.min.js?ver=1.2.0"></script>
+  <script src="../assets/scripts/aos.js?ver=1.2.0"></script>
+  <script src="../assets/scripts/main.js?ver=1.2.0"></script>
+</body>
+
+</html>
